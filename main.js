b2be089c70782ed79ef36a148b8bcd4aa74af189
facundopandol <facundopandol90@gmail.com>
2023-05-01 20:56:21 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+function convertirTemperatura(temperatura, temp1, temp2) {
+    if (temp1 === "celsius" && temp2 === "kelvin") {
+        return temperatura + 273.15;
+    } else if (temp1 === "celsius" && temp2 === "farenheit") {
+        return temperatura * 1.8 + 32;
+    } else if (temp1 === "kelvin" && temp2 === "celsius") {
+        return temperatura - 273.15;
+    } else if (temp1 === "kelvin" && temp2 === "farenheit") {
+        return (temperatura - 273.15) * 1.8 + 32;
+    } else if (temp1 === "farenheit" && temp2 === "kelvin") {
+        return ((temperatura - 32) * 5) / 9 + 273.15;
+    } else if (temp1 === "farenheit" && temp2 === "celsius") {
+        return (temperatura - 32) / 1.8;
+    } else {
+        return "El dato ingresado no pudo convertirse";
+
+    }
+}
+
+let bienvenida = prompt("Quieres hacer una conversión? (s/n)")
+
+while (bienvenida === "s") {
+
+    let temperatura = parseFloat(prompt("Escribe la temperatura a convertir"));
+    let temp1 = prompt("Ingrese la unidad a convertir (celsius, kelvin, farenheit)").toLowerCase();
+    let temp2 = prompt("Ingrese la unidad del resultado (celsius, kelvin, farenheit").toLowerCase();
+    let resultado = convertirTemperatura(temperatura, temp1, temp2);
+
+    if (temperatura === NaN) {
+        alert("La temperatura ingresada no es válida");
+    } else if (
+        (temp1 !== "celsius" && temp1 !== "kelvin" && temp1 !== "farenheit") ||
+        (temp2 !== "celsius" && temp2 !== "kelvin" && temp2 !== "farenheit")
+    ) {
+        alert("Las unidades ingresadas no son válidas");
+    } else {
+        alert(`Tu resultado es ${resultado} grados ${temp2}`);
+    }
+    bienvenida = prompt("Quieres hacer otra conversión? (s/n)")
+}
+alert("Gracias por usar nuestro sistema de conversión de unidades de temperatura");
